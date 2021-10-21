1579fbb559490420b5e8f8392f92ba430bd94cb9
Caique Giovannini <caique_giovannini@hotmail.com>
2021-10-21 12:10:22 -0300

    Add api methods

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,12 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
 });
+
+export const getAllProducts = async () => (await api.get('/products')).data;
+export const getProduct = async (productId: number) => (await api.get(`/products/${productId}`)).data;
+
+export const getProductStock = async (productId: number) => (await api.get(`/stock/${productId}`)).data;
+export const decreaseProductStock = async (productId: number, productAmount: number) => await api.put(`/stock/${productId}`, {
+  id: productId,
+  amount: productAmount,
+});
